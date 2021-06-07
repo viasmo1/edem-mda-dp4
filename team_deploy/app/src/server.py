d7e6de0bb27852a12435b151102243ebdb83163b
Javi Briones <jbg922@gmail.com>
2021-06-07 23:44:43 +0200

    add output & update files

--- team_deploy/app/src/server.py
+++ team_deploy/app/src/server.py
@@ -5,8 +5,12 @@ from swagger.app import blueprint as app_endpoints
 import base64
 from datetime import datetime
 
-# load model
-model = pickle.load(open('models/model.pkl','rb'))
+import tensorflow as tf
+import numpy as np
+from keras import backend
+from tensorflow.keras import models
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 
 # app
 app = Flask(__name__)
@@ -39,15 +43,39 @@ def predict():
             file_to_save.write(decoded_image_data)
 
         #Output prediction
-        # convert data into dataframe
-        #data.update((x, [y]) for x, y in data.items())
-        #data_df = pd.DataFrame.from_dict(data)
+        # calculate fbeta score for multi-class/label classification
+        def fbeta(y_true, y_pred, beta=2):
+            # clip predictions
+            y_pred = backend.clip(y_pred, 0, 1)
+            # calculate elements
+            tp = backend.sum(backend.round(backend.clip(y_true * y_pred, 0, 1)), axis=1)
+            fp = backend.sum(backend.round(backend.clip(y_pred - y_true, 0, 1)), axis=1)
+            fn = backend.sum(backend.round(backend.clip(y_true - y_pred, 0, 1)), axis=1)
+            # calculate precision
+            p = tp / (tp + fp + backend.epsilon())
+            # calculate recall
+            r = tp / (tp + fn + backend.epsilon())
+            # calculate fbeta, averaged across each class
+            bb = beta ** 2
+            fbeta_score = backend.mean((1 + bb) * (p * r) / (bb * p + r + backend.epsilon()))
+            return fbeta_score
 
         # predictions
-        #result = model.predict(data_df)
+        #Load Model
+        new_model = tf.keras.models.load_model('./models/score_3249.h5', custom_objects={'fbeta': fbeta})
 
-        # send back to browser
-        output = {'results': int(15)}
+        #Load image
+        photo = load_img('../../bucket_images/'+file_name_st+'.png', target_size=(128,128))
+        # convert to numpy array
+        photo = img_to_array(photo, dtype='uint8')
+
+        X = np.reshape(photo, (1,128,128,3))
+
+        # output
+        tags = new_model.predict(X)
+        tags.argmax()
+        list_emotions = ['Affection','Anger','Annoyance', 'Anticipation', 'Aversion', 'Confidence', 'Disapproval', 'Disconnection', 'Disquietment', 'Doubt/Confusion', 'Embarrassment', 'Engagement', 'Esteem', 'Excitement', 'Fatigue', 'Fear', 'Happiness', 'Pain', 'Peace', 'Pleasure', 'Sadness', 'Sensitivity', 'Suffering', 'Surprise', 'Sympathy', 'Yearning']
+        output = {'emotions': list_emotions[tags.argmax()], "score": 0.3249}
 
         # return data
         return jsonify(results=output)
--- team_deploy/test_api_services/host.py
+++ team_deploy/test_api_services/host.py
@@ -1,12 +1,13 @@
 import base64
 import requests
+import datetime
 
 def get_method():
     r = requests.get('http://127.0.0.1:5000/prediction')
     print(r.text)
 
 def return_json(str_name, str_img):
-    return '{"creationdata":'+str_name+','+'"message":'+'"'+str_img+'"'+'}'
+    return '{"creationDate":'+'"'+str_name+'"'+','+'"message":'+'"'+str_img+'"'+'}'
  
 def post_method():
     with open('presi.jpg', 'rb') as binary_file:
@@ -14,7 +15,9 @@ def post_method():
             base64_encoded_data = base64.b64encode(binary_file_data)
             base64_message = base64_encoded_data.decode('utf-8')
 
-    data_in = return_json('2016',base64_message)
+    
+    creationDate = str(datetime.datetime.now())
+    data_in = return_json(creationDate, base64_message)
     #data_in = return_json_val()
     print(data_in)
     r = requests.post("http://127.0.0.1:5000/prediction", data=data_in)
