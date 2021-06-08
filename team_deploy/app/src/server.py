e75f8f1023dd1955e49faf4a8c644f40c0090710
Javi Briones <jbg922@gmail.com>
2021-06-08 18:46:03 +0200

    update server.py

--- team_deploy/app/src/server.py
+++ team_deploy/app/src/server.py
@@ -20,6 +20,22 @@ app.register_blueprint(app_endpoints)
 # routes
 @app.route('/prediction', methods=['GET', 'POST'])
 
+def fbeta(y_true, y_pred, beta=2):
+            # clip predictions
+            y_pred = backend.clip(y_pred, 0, 1)
+            # calculate elements
+            tp = backend.sum(backend.round(backend.clip(y_true * y_pred, 0, 1)), axis=1)
+            fp = backend.sum(backend.round(backend.clip(y_pred - y_true, 0, 1)), axis=1)
+            fn = backend.sum(backend.round(backend.clip(y_true - y_pred, 0, 1)), axis=1)
+            # calculate precision
+            p = tp / (tp + fp + backend.epsilon())
+            # calculate recall
+            r = tp / (tp + fn + backend.epsilon())
+            # calculate fbeta, averaged across each class
+            bb = beta ** 2
+            fbeta_score = backend.mean((1 + bb) * (p * r) / (bb * p + r + backend.epsilon()))
+            return fbeta_score
+
 def predict():
 
     if request.method == "GET":
@@ -43,22 +59,6 @@ def predict():
             file_to_save.write(decoded_image_data)
 
         #Output prediction
-        # calculate fbeta score for multi-class/label classification
-        def fbeta(y_true, y_pred, beta=2):
-            # clip predictions
-            y_pred = backend.clip(y_pred, 0, 1)
-            # calculate elements
-            tp = backend.sum(backend.round(backend.clip(y_true * y_pred, 0, 1)), axis=1)
-            fp = backend.sum(backend.round(backend.clip(y_pred - y_true, 0, 1)), axis=1)
-            fn = backend.sum(backend.round(backend.clip(y_true - y_pred, 0, 1)), axis=1)
-            # calculate precision
-            p = tp / (tp + fp + backend.epsilon())
-            # calculate recall
-            r = tp / (tp + fn + backend.epsilon())
-            # calculate fbeta, averaged across each class
-            bb = beta ** 2
-            fbeta_score = backend.mean((1 + bb) * (p * r) / (bb * p + r + backend.epsilon()))
-            return fbeta_score
 
         # predictions
         #Load Model
