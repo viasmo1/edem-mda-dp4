e7ebee72b21c0a0c34356e62f42a8c7ad6b8b506
Vicent Asensio <vicent.asenmol@gmail.com>
2021-06-10 07:48:22 +0200

    Update ingestion file and requirements

--- team_arch/src/ingestion.py
+++ team_arch/src/ingestion.py
@@ -2,9 +2,29 @@ import os
 import requests
 from zipfile import ZipFile
 from io import BytesIO
+import numpy as np
+import pandas as pd
+import cv2
 
 url = "https://github.com/lggx/Face_expression_recognition/raw/main/fer2013.zip"
 response = requests.get(url)
 
 z = ZipFile(BytesIO(response.content))
 z.extractall("./images/initial_dataset")
+
+os.chdir("./images/initial_dataset")
+
+"""
+data = pd.read_csv("fer2013.csv")
+
+data["pixels_graph"] = data["pixels"].apply(lambda x: x.split(" "))
+data["pixels_graph"] = data["pixels_graph"].apply(lambda x: np.reshape(x, (48, 48)))
+data["pixels_graph"] = data["pixels_graph"].apply(lambda x: x.astype(int))
+
+data.to_csv("fer2013_images.csv", index=False)
+
+
+for index, row in data.iterrows():
+    pic_name = f"{index}.jpg"
+    cv2.imwrite(pic_name, row["pixels_graph"])
+"""
