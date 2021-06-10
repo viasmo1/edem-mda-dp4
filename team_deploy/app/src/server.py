e949c01876f6c81d25012b9d670de01417c04076
Jordi Oltra <jordioltra2@gmail.com>
2021-06-10 22:06:34 +0200

    ya funciona

--- team_deploy/app/src/server.py
+++ team_deploy/app/src/server.py
@@ -13,6 +13,8 @@ from keras import backend
 from tensorflow.keras import models
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
+from PIL import Image
+import cv2
 
 
 
@@ -25,21 +27,21 @@ app.register_blueprint(app_endpoints)
 # routes
 @app.route('/prediction', methods=['GET', 'POST'])
 
-def fbeta(y_true, y_pred, beta=2):
-            # clip predictions
-            y_pred = backend.clip(y_pred, 0, 1)
-            # calculate elements
-            tp = backend.sum(backend.round(backend.clip(y_true * y_pred, 0, 1)), axis=1)
-            fp = backend.sum(backend.round(backend.clip(y_pred - y_true, 0, 1)), axis=1)
-            fn = backend.sum(backend.round(backend.clip(y_true - y_pred, 0, 1)), axis=1)
-            # calculate precision
-            p = tp / (tp + fp + backend.epsilon())
-            # calculate recall
-            r = tp / (tp + fn + backend.epsilon())
-            # calculate fbeta, averaged across each class
-            bb = beta ** 2
-            fbeta_score = backend.mean((1 + bb) * (p * r) / (bb * p + r + backend.epsilon()))
-            return fbeta_score
+#def fbeta(y_true, y_pred, beta=2):
+#            # clip predictions
+#            y_pred = backend.clip(y_pred, 0, 1)
+#            # calculate elements
+#            tp = backend.sum(backend.round(backend.clip(y_true * y_pred, 0, 1)), axis=1)
+#            fp = backend.sum(backend.round(backend.clip(y_pred - y_true, 0, 1)), axis=1)
+#            fn = backend.sum(backend.round(backend.clip(y_true - y_pred, 0, 1)), axis=1)
+#            # calculate precision
+#            p = tp / (tp + fp + backend.epsilon())
+#            # calculate recall
+#            r = tp / (tp + fn + backend.epsilon())
+#            # calculate fbeta, averaged across each class
+#            bb = beta ** 2
+#            fbeta_score = backend.mean((1 + bb) * (p * r) / (bb * p + r + backend.epsilon()))
+#            return fbeta_score
 
 def predict():
 
@@ -59,7 +61,7 @@ def predict():
         base64_img_bytes = img.encode('utf-8')
         now = datetime.now()
         file_name_st = now.strftime("%d%m%Y%H%M%S")
-        with open('../../bucket_images/'+file_name_st+'.png', 'wb') as file_to_save:
+        with open('../../images/'+file_name_st+'.png', 'wb') as file_to_save: #bucket_
             decoded_image_data = base64.decodebytes(base64_img_bytes)
             file_to_save.write(decoded_image_data)
 
@@ -67,23 +69,29 @@ def predict():
 
         # predictions
         #Load Model
-        with open('FacialExpression-model.json', 'r') as json_file:
+        with open(r'C:\Users\20rd1\Desktop\computer_vision\edem-mda-dp4\team_deploy\app\src\models\FacialExpression-model.json', 'r') as json_file:
             json_savedModel= json_file.read()
 
             # Cargamos la arquitectura del modelo
         new_model = tf.keras.models.model_from_json(json_savedModel)
-        new_model.load_weights('FacialExpression_weights.hdf5')
+        new_model.load_weights(r'C:\Users\20rd1\Desktop\computer_vision\edem-mda-dp4\team_deploy\app\src\models\FacialExpression_weights.hdf5')
         new_model.compile(optimizer = "Adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
 
         #Load image
-        photo = load_img('../../bucket_images/'+file_name_st+'.png', target_size=(128,128))
+        def load_image( infilename ) :
+            img = Image.open( infilename )
+            img.load()
+            data = np.asarray( img, dtype="int32" )
+            return data
+        #photo = load_img('../../images/'+file_name_st+'.png', target_size=(128,128)) #bucket_
+        photo = load_image('../../images/'+file_name_st+'.png') #'../../images/'+file_name_st+'.png'
         # convert to numpy array
 
         # Blanco y negro
         imgGray = color.rgb2gray(photo)
 
         # 1er redimensionado
-        resize_img = imgGray.reshape(96,96)
+        resize_img = cv2.resize(imgGray  , (96 , 96))
 
         #redimensionado
         resize_img2 = resize_img.reshape(1,96,96,1)
